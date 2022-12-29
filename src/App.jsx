6a7d67e4bc87a87083726c9eb2988a9bbd34c139
cleverAkanimoh <crushclever1@gmail.com>
2022-12-29 05:12:26 +0100

    modified 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     return (
         <main>
            <Navbar />
-           <Hero />
+           {/* <Hero /> */}
            <Experiences />
         </main>
     )
--- src/Components/Experiences.jsx
+++ src/Components/Experiences.jsx
@@ -1,8 +1,43 @@
+import image from '../images/katie-zaferes.png';
+import star from '../images/star.png';
+
 const Experiences = () => {
     return (
-        <article>
-            <h2>Experiences</h2>
-        </article>
+        <section id="card">
+            <article class="article--container">
+                <img src={image} alt="katie-zaferes" class="card--img" />
+                <div className='article--div'>
+                    <img src={star} alt="star" />
+                    <small>5.0</small>&nbsp;
+                    <samp>(6)&circ;</samp>
+                    <abbr>USA</abbr>
+                </div>
+                <p>Life lessons with Katie Zaferes</p>
+                <p><strong>From $136</strong> / person</p>
+            </article>
+            <article class="article--container">
+                <img src={image} alt="katie-zaferes" class="card--img" />
+                <div className='article--div'>
+                    <img src={star} alt="star" />
+                    <small>5.0</small>&nbsp;
+                    <samp>(6)&circ;</samp>
+                    <abbr>USA</abbr>
+                </div>
+                <p>Life lessons with Katie Zaferes</p>
+                <p><strong>From $136</strong> / person</p>
+            </article>
+            <article class="article--container">
+                <img src={image} alt="katie-zaferes" class="card--img" />
+                <div className='article--div'>
+                    <img src={star} alt="star" />
+                    <small>5.0</small>&nbsp;
+                    <samp>(6)&circ;</samp>
+                    <abbr>USA</abbr>
+                </div>
+                <p>Life lessons with Katie Zaferes</p>
+                <p><strong>From $136</strong> / person</p>
+            </article>
+        </section>
     );
 }
 
