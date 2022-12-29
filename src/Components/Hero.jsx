f44e8758479dd4b2a97fa6d18c731665e6a4f8da
cleverAkanimoh <crushclever1@gmail.com>
2022-12-29 03:46:34 +0100

    modified Hero.jsx and index.css

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <section className="hero">
             <img src={gridImage} alt='grid photos' className="hero--photo" />
-            <h1 className="hero--header">Online Experiences</h1>
+            <h2 className="hero--header">Online Experiences</h2>
             <p className="hero--text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
         </section>
